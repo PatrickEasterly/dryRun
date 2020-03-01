53bed8f79648bf0ee32b76c67bd9a5fbca909134
patrick <pateast678@gmail.com>
2020-03-01 13:47:32 -0500

    Adds a nice little landing screen

--- App.js
+++ App.js
@@ -1,55 +1,68 @@
-import * as React from 'react';
-import { Button, Image, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import React from 'react';
+// "View is a container that supports layout with flexbox, style, some touch handling, and accessibility controls"
+// "Text: A React component for displaying text."
+import {View, Text, ImageBackground} from 'react-native';
+import {Button} from 'native-base';
 
-export default class ImagePickerExample extends React.Component {
-  state = {
-    image: null,
-  };
+// To use the image, you need to require it
+const background = require('./assets/bachgrund.png');
+// Use backgroundimage component so we can have child elements
 
+export default class App extends React.Component {
   render() {
-    let { image } = this.state;
-
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-        {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <View style={styles.container}>
+        <ImageBackground source={background} style={styles.background}>
+        <View style={styles.itemContainer}>
+            <Text style={styles.titleStyle}>Hello, expo</Text>
+            <Button
+            onPress={()=>console.log('f')}
+            block={true}  
+            // This is provided by native-base. block={true} makes a nice wide button that spans the screen
+            style={styles.buttonStyle}
+            
+            >
+              <Text style={styles.buttonText}>enter iPlant</Text>
+            </Button>
+          </View>
+        </ImageBackground>
       </View>
-    );
+    )
   }
+}
 
-  componentDidMount() {
-    this.getPermissionAsync();
-    console.log('hi');
+// This is the way they do styles in the docs--make a styles object, 
+// give the component inline styling that references this 
+// <View style={styles.whatever}>
+
+
+const styles = {
+  titleStyle: {
+    fontSize: 30,
+    color: 'blue'
+  },
+  background: {
+    flex: 1         // We really want this, so the background takes up the whole background
+  },
+  container: {
+    flex: 1,        // This makes the app span the screen
+    // marginTop: 24   // Gives a little whitespace at the top of the screen
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  clickMe: {
+    flex: 1,
+    color: 'blue',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#0f0'
+  },
+  buttonStyle: {
+    margin: 10
   }
-
-  getPermissionAsync = async () => {
-    if (Constants.platform.ios) {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    }
-  }
-
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
-    }
-  };
 }
